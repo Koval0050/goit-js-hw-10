2a7d089519235bc8620af6dd49c48f64bb171802
Koval0050 <121100028+Koval0050@users.noreply.github.com>
2023-06-12 12:46:50 +0300

    hw_10_v1

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,4 +1,4 @@
-function fetchBreeds() {
+const fetchBreeds = () => {
   const url = `https://api.thecatapi.com/v1/breeds`;
   const api_key =
     ' live_AqVAdbSI3iq6qvheU2btDbPvPUx4FhPf0KO84Ew4BtwLMszLCefwpjCjD6X1ksjH';
@@ -13,12 +13,39 @@ function fetchBreeds() {
       return response.json();
     })
     .then(data => {
+      // console.log(data);
       //filter to only include those with an `image` object
-      storedBreeds = data.map(e => e.name);
-      console.log(storedBreeds);
-    })
-    .catch(function (error) {
-      console.log(error);
+      storedBreeds = data.map(e => {
+        const nameId = { name: e.name, id: e.id };
+        return nameId;
+      });
+      return storedBreeds;
     });
-}
-export default fetchBreeds;
+};
+
+const fetchCatByBreed = breedId => {
+  const api_key =
+    ' live_AqVAdbSI3iq6qvheU2btDbPvPUx4FhPf0KO84Ew4BtwLMszLCefwpjCjD6X1ksjH';
+  const url = `https://api.thecatapi.com/v1/breeds/${breedId}`;
+
+  return fetch(url, {
+    headers: {
+      'x-api-key': api_key,
+    },
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      const selectCat = {
+        name: data.name,
+        temperament: data.temperament,
+        description: data.description,
+        imgAlt: data.alt_names,
+        img: data.reference_image_id,
+      };
+      return selectCat;
+    });
+};
+
+export { fetchBreeds, fetchCatByBreed };
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import fetchBreeds from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const refs = {
   breedSelect: document.querySelector('.breed-select'),
@@ -6,17 +6,77 @@ const refs = {
   error: document.querySelector('.error'),
   catInfo: document.querySelector('.cat-info'),
 };
+refs.breedSelect.addEventListener('change', e => {
+  // Hide any previous error message
+  refs.error.classList.remove('visible');
 
-fetchBreeds();
+  // Show the loader
+  refs.loader.classList.add('visible');
+  refs.catInfo.classList.add('el_disp_none');
 
-// function showSelect(massName) {
-//   const option = massName.map(e => {
-//     return `<option id="${e.id}" value="${e.name}">${e.name}</option>`;
-//   });
-//   return option;
-// }
-// data = data.filter(img => img.image?.url != null);
-// storedBreeds = data;
-// console.log(storedBreeds);
-// const showSelectName = showSelect(storedBreeds);
-// refs.breedSelect.insertAdjacentHTML('beforeend', showSelectName);
+  const optionValue = e.target.options[e.target.selectedIndex].value;
+  fetchCatByBreed(optionValue)
+    .then(data => {
+      if (!data.name || !data.temperament || !data.description) {
+        throw data;
+      }
+      return data;
+    })
+    .then(({ name, temperament, description, imgAlt, img }) => {
+      const cat = `<div class = "container"><h2>${name || 'cat name'}</h2>
+    <p><b>Temperament: </b>${temperament}</p>
+    <img src="https://cdn2.thecatapi.com/images/${img}.jpg" width="500" alt="${
+        imgAlt || 'cat'
+      }">
+    <p>${description || 'description'}</p></div>`;
+      refs.catInfo.classList.remove('el_disp_none');
+
+      refs.catInfo.innerHTML = cat;
+    })
+    .catch(error => {
+      console.error('Якогось із параметрів не хватає:', error);
+      alert(
+        'Упс, схоже виникла помилка, спробуйте перезавантажити сторінку, якщо це не допоможе то спробуйте обрати іншого котика'
+      );
+      refs.catInfo.classList.add('el_disp_none');
+
+      // Show the error message
+      refs.error.classList.add('visible');
+    })
+    .finally(() => {
+      // Hide the loader
+      refs.loader.classList.remove('visible');
+    });
+});
+
+// Show the loader when fetching the breed list
+refs.loader.classList.add('visible');
+
+fetchBreeds()
+  .then(data => {
+    const option = showSelect(data);
+    refs.breedSelect.insertAdjacentHTML('beforeend', option.join(''));
+  })
+  .catch(error => {
+    refs.loader.classList.remove('el_disp_none');
+
+    refs.loader.classList.add('visible');
+
+    alert(
+      'Упс, схоже сайт не працює або у вас проблеми з інтернетом, спробуйте перезавантажити сторінку'
+    );
+    // console.error('er', error);
+  })
+  .finally(() => {
+    // Hide the loader
+    refs.loader.classList.remove('visible');
+    // Show the breed select
+    refs.breedSelect.classList.remove('el_disp_none');
+  });
+
+function showSelect(massName) {
+  const option = massName.map(e => {
+    return `<option value="${e.id}">${e.name}</option>`;
+  });
+  return option;
+}
